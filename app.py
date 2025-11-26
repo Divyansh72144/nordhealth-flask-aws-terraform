4f7c6689c1dfd1884374f7f128166780c53c8791
divyansh72144 <divyansh72144@gmail.com>
2025-11-26 15:46:49 +0200

    Added Prometheus for request timing and adding  metrics endpoint

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from prometheus_client import start_http_server, Summary
 
 app = Flask(__name__)
 
+REQUEST_TIME = Summary('hello_world_request_processing_seconds', 'Time spent processing hello world requests')
+
 @app.route('/', methods=['GET'])
+@REQUEST_TIME.time()
 def home():
-    return "Hello World!"
+    return "Hello World! Prometheus metrics can be viewed at :8000/metrics"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+    start_http_server(8000)
+    app.run(host='0.0.0.0', port=8080)
